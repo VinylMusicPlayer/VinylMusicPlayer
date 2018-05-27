1edabc3f239140d2672cdf8b913a2cc12661c065
Martin Disch <martindisch@gmail.com>
2018-05-27 13:35:53 +0200

    Run bulk export in AsyncTask
    # Conflicts:
    #	app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/mainactivity/library/LibraryFragment.java

--- app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/mainactivity/library/LibraryFragment.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/mainactivity/library/LibraryFragment.java
@@ -1,9 +1,12 @@
 package com.poupa.vinylmusicplayer.ui.fragments.mainactivity.library;
 
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,6 +22,7 @@ import android.view.MenuItem;
 import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.afollestad.materialcab.MaterialCab;
 import com.kabouzeid.appthemehelper.ThemeStore;
@@ -40,6 +44,7 @@ import com.poupa.vinylmusicplayer.ui.fragments.mainactivity.library.pager.Albums
 import com.poupa.vinylmusicplayer.ui.fragments.mainactivity.library.pager.ArtistsFragment;
 import com.poupa.vinylmusicplayer.ui.fragments.mainactivity.library.pager.PlaylistsFragment;
 import com.poupa.vinylmusicplayer.ui.fragments.mainactivity.library.pager.SongsFragment;
+import com.poupa.vinylmusicplayer.util.PlaylistsUtil;
 import com.poupa.vinylmusicplayer.util.PreferenceUtil;
 import com.poupa.vinylmusicplayer.util.Util;
 import com.poupa.vinylmusicplayer.util.VinylMusicPlayerColorUtil;
@@ -247,7 +252,29 @@ public class LibraryFragment extends AbsMainActivityFragment implements CabHolde
                 CreatePlaylistDialog.create().show(getChildFragmentManager(), "CREATE_PLAYLIST");
                 return true;
             case R.id.action_export_playlists:
-                // TODO
+                @SuppressLint("ShowToast")
+                final Toast toast = Toast.makeText(getActivity(), R.string.saving_playlists, Toast.LENGTH_LONG);
+                new AsyncTask<Context, Void, String>() {
+                    @Override
+                    protected void onPreExecute() {
+                        super.onPreExecute();
+                        toast.show();
+                    }
+
+                    @Override
+                    protected String doInBackground(Context... params) {
+                        return PlaylistsUtil.saveAllPlaylists(params[0]);
+                    }
+
+                    @Override
+                    protected void onPostExecute(String string) {
+                        super.onPostExecute(string);
+                        if (toast != null) {
+                            toast.setText(string);
+                            toast.show();
+                        }
+                    }
+                }.execute(getActivity().getApplicationContext());
                 return true;
             case R.id.action_search:
                 startActivity(new Intent(getActivity(), SearchActivity.class));
