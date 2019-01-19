ellienieuwdorp/gelli

Move some util functions

app/src/main/java/com/kabouzeid/gramophone/adapter/SongFileAdapter.java
@@ -20,7 +20,7 @@ import com.kabouzeid.gramophone.adapter.base.AbsMultiSelectAdapter;
 import com.kabouzeid.gramophone.adapter.base.MediaEntryViewHolder;
 import com.kabouzeid.gramophone.glide.audiocover.AudioFileCover;
 import com.kabouzeid.gramophone.interfaces.CabHolder;
-import com.kabouzeid.gramophone.util.Util;
+import com.kabouzeid.gramophone.util.ImageUtil;
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
 
 import java.io.File;
@@ -116,7 +116,7 @@ public class SongFileAdapter extends AbsMultiSelectAdapter<SongFileAdapter.ViewH
             holder.image.setColorFilter(iconColor, PorterDuff.Mode.SRC_IN);
             holder.image.setImageResource(R.drawable.ic_folder_white_24dp);
         } else {
-            Drawable error = Util.getTintedVectorDrawable(activity, R.drawable.ic_file_music_white_24dp, iconColor);
+            Drawable error = ImageUtil.getTintedVectorDrawable(activity, R.drawable.ic_file_music_white_24dp, iconColor);
             Glide.with(activity)
                     .load(new AudioFileCover(file.getPath()))
                     .diskCacheStrategy(DiskCacheStrategy.NONE)

app/src/main/java/com/kabouzeid/gramophone/appshortcuts/AppShortcutIconGenerator.java
@@ -1,8 +1,6 @@
 package com.kabouzeid.gramophone.appshortcuts;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.drawable.AdaptiveIconDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
@@ -14,14 +12,15 @@ import android.util.TypedValue;
 
 import com.kabouzeid.appthemehelper.ThemeStore;
 import com.kabouzeid.gramophone.R;
+import com.kabouzeid.gramophone.util.ImageUtil;
 import com.kabouzeid.gramophone.util.PreferenceUtil;
-import com.kabouzeid.gramophone.util.Util;
 
 /**
  * @author Adrian Campos
  */
 @RequiresApi(Build.VERSION_CODES.N_MR1)
 public final class AppShortcutIconGenerator {
+
     public static Icon generateThemedIcon(Context context, int iconId) {
         if (PreferenceUtil.getInstance(context).coloredAppShortcuts()) {
             return generateUserThemedIcon(context, iconId).toIcon();
@@ -52,26 +51,19 @@ public final class AppShortcutIconGenerator {
 
     private static IconCompat generateThemedIcon(Context context, int iconId, int foregroundColor, int backgroundColor) {
         // Get and tint foreground and background drawables
-        Drawable vectorDrawable = Util.getTintedVectorDrawable(context, iconId, foregroundColor);
-        Drawable backgroundDrawable = Util.getTintedVectorDrawable(context, R.drawable.ic_app_shortcut_background, backgroundColor);
+        Drawable vectorDrawable = ImageUtil.getTintedVectorDrawable(context, iconId, foregroundColor);
+        Drawable backgroundDrawable = ImageUtil.getTintedVectorDrawable(context, R.drawable.ic_app_shortcut_background, backgroundColor);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             AdaptiveIconDrawable adaptiveIconDrawable = new AdaptiveIconDrawable(backgroundDrawable, vectorDrawable);
-            return IconCompat.createWithAdaptiveBitmap(drawableToBitmap(adaptiveIconDrawable));
+            return IconCompat.createWithAdaptiveBitmap(ImageUtil.createBitmap(adaptiveIconDrawable));
         } else {
             // Squash the two drawables together
             LayerDrawable layerDrawable = new LayerDrawable(new Drawable[]{backgroundDrawable, vectorDrawable});
 
             // Return as an Icon
-            return IconCompat.createWithBitmap(drawableToBitmap(layerDrawable));
+            return IconCompat.createWithBitmap(ImageUtil.createBitmap(layerDrawable));
         }
     }
 
-    private static Bitmap drawableToBitmap(Drawable drawable) {
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
-    }
 }

VinylMusicPlayer/VinylMusicPlayer

app/src/main/java/com/poupa/vinylmusicplayer/adapter/SongFileAdapter.java
@@ -22,6 +22,7 @@ import com.poupa.vinylmusicplayer.adapter.base.MediaEntryViewHolder;
 import com.poupa.vinylmusicplayer.glide.GlideApp;
 import com.poupa.vinylmusicplayer.glide.audiocover.AudioFileCover;
 import com.poupa.vinylmusicplayer.interfaces.CabHolder;
+import com.poupa.vinylmusicplayer.util.ImageUtil;
 import com.poupa.vinylmusicplayer.util.Util;
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
 
@@ -118,7 +119,7 @@ public class SongFileAdapter extends AbsMultiSelectAdapter<SongFileAdapter.ViewH
             holder.image.setColorFilter(iconColor, PorterDuff.Mode.SRC_IN);
             holder.image.setImageResource(R.drawable.ic_folder_white_24dp);
         } else {
-            Drawable error = Util.getTintedVectorDrawable(activity, R.drawable.ic_file_music_white_24dp, iconColor);
+            Drawable error = ImageUtil.getTintedVectorDrawable(activity, R.drawable.ic_file_music_white_24dp, iconColor);
             GlideApp.with(activity)
                     .load(new AudioFileCover(file.getPath()))
                     .transition(GenericTransitionOptions.with(android.R.anim.fade_in))

app/src/main/java/com/poupa/vinylmusicplayer/appshortcuts/AppShortcutIconGenerator.java
@@ -1,8 +1,6 @@
 package com.poupa.vinylmusicplayer.appshortcuts;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.drawable.AdaptiveIconDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
@@ -15,13 +13,14 @@ import android.util.TypedValue;
 import com.kabouzeid.appthemehelper.ThemeStore;
 import com.poupa.vinylmusicplayer.R;
 import com.poupa.vinylmusicplayer.util.PreferenceUtil;
-import com.poupa.vinylmusicplayer.util.Util;
+import com.poupa.vinylmusicplayer.util.ImageUtil;
 
 /**
  * @author Adrian Campos
  */
 @RequiresApi(Build.VERSION_CODES.N_MR1)
 public final class AppShortcutIconGenerator {
+
     public static Icon generateThemedIcon(Context context, int iconId) {
         if (PreferenceUtil.getInstance().coloredAppShortcuts()) {
             return generateUserThemedIcon(context, iconId).toIcon();
@@ -52,26 +51,19 @@ public final class AppShortcutIconGenerator {
 
     private static IconCompat generateThemedIcon(Context context, int iconId, int foregroundColor, int backgroundColor) {
         // Get and tint foreground and background drawables
-        Drawable vectorDrawable = Util.getTintedVectorDrawable(context, iconId, foregroundColor);
-        Drawable backgroundDrawable = Util.getTintedVectorDrawable(context, R.drawable.ic_app_shortcut_background, backgroundColor);
+        Drawable vectorDrawable = ImageUtil.getTintedVectorDrawable(context, iconId, foregroundColor);
+        Drawable backgroundDrawable = ImageUtil.getTintedVectorDrawable(context, R.drawable.ic_app_shortcut_background, backgroundColor);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             AdaptiveIconDrawable adaptiveIconDrawable = new AdaptiveIconDrawable(backgroundDrawable, vectorDrawable);
-            return IconCompat.createWithAdaptiveBitmap(drawableToBitmap(adaptiveIconDrawable));
+            return IconCompat.createWithAdaptiveBitmap(ImageUtil.createBitmap(adaptiveIconDrawable));
         } else {
             // Squash the two drawables together
             LayerDrawable layerDrawable = new LayerDrawable(new Drawable[]{backgroundDrawable, vectorDrawable});
 
             // Return as an Icon
-            return IconCompat.createWithBitmap(drawableToBitmap(layerDrawable));
+            return IconCompat.createWithBitmap(ImageUtil.createBitmap(layerDrawable));
         }
     }
 
-    private static Bitmap drawableToBitmap(Drawable drawable) {
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
-    }
 }