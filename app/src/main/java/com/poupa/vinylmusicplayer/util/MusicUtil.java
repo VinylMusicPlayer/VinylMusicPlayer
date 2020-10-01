fbe01ac9f505028ee37522778319444b3ed27c01
Son <son.caokim@gmail.com>
2020-10-01 22:19:49 +0200

    Case insensitive for unknown album/artist/genre detection

--- app/src/main/java/com/poupa/vinylmusicplayer/util/MusicUtil.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/util/MusicUtil.java
@@ -378,9 +378,8 @@ public class MusicUtil {
 
     private static boolean isNameUnknown(@Nullable String name, @NonNull final String defaultDisplayName) {
         if (TextUtils.isEmpty(name)) return true;
-        if (name.equals(defaultDisplayName)) return true;
         name = name.trim().toLowerCase();
-        return name.equals("unknown") || name.equals("<unknown>");
+        return (name.equals(defaultDisplayName) || name.equals("unknown") || name.equals("<unknown>"));
     }
 
     @NonNull
