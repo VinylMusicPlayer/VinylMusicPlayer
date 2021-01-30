1031d81ba02d5826dd4f7806239c8e3b438b9985
Son <son.caokim@gmail.com>
2021-01-30 21:30:48 +0100

    Refact top tracks to rely on discog

--- app/src/main/java/com/poupa/vinylmusicplayer/loader/TopAndRecentlyPlayedTracksLoader.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/loader/TopAndRecentlyPlayedTracksLoader.java
@@ -18,7 +18,6 @@ package com.poupa.vinylmusicplayer.loader;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.provider.BaseColumns;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -68,7 +67,11 @@ public class TopAndRecentlyPlayedTracksLoader {
 
     @NonNull
     public static ArrayList<Song> getTopTracks(@NonNull Context context) {
-        return SongLoader.getSongs(makeTopTracksCursorAndClearUpDatabase(context));
+        final int NUMBER_OF_TOP_TRACKS = 100;
+        try (Cursor cursor = SongPlayCountStore.getInstance(context).getTopPlayedResults(NUMBER_OF_TOP_TRACKS)){
+            ArrayList<Long> songIds = getIdsFromCursor(cursor, SongPlayCountStore.SongPlayCountColumns.ID);
+            return getSongsFromIdsAndCleanupHistory(context, songIds);
+        }
     }
 
     @NonNull
@@ -103,70 +106,5 @@ public class TopAndRecentlyPlayedTracksLoader {
 
         HistoryStore.getInstance(context).removeSongIds(orphanSongIds);
         return songs;
-    }
-
-    private static void cleanupDatabase(@NonNull final Context context, @Nullable final SortedLongCursor cursor) {
-        if (cursor != null) {
-            HistoryStore.getInstance(context).removeSongIds(cursor.getMissingIds());
-        }
-    }
-
-    @Nullable
-    public static Cursor makeTopTracksCursorAndClearUpDatabase(@NonNull final Context context) {
-        SortedLongCursor retCursor = makeTopTracksCursorImpl(context);
-        cleanupDatabase(context, retCursor);
-
-        return retCursor;
-    }
-
-    @Nullable
-    private static SortedLongCursor makeTopTracksCursorImpl(@NonNull final Context context) {
-        final int NUMBER_OF_TOP_TRACKS = 100;
-        Cursor songs = SongPlayCountStore.getInstance(context).getTopPlayedResults(NUMBER_OF_TOP_TRACKS);
-
-        try {
-            return makeSortedCursor(context, songs,
-                    songs.getColumnIndex(SongPlayCountStore.SongPlayCountColumns.ID));
-        } finally {
-            if (songs != null) {
-                songs.close();
-            }
-        }
-    }
-
-    @Nullable
-    private static SortedLongCursor makeSortedCursor(@NonNull final Context context, @Nullable final Cursor cursor, final int idColumn) {
-        if (cursor != null && cursor.moveToFirst()) {
-            // create the list of ids to select against
-            StringBuilder selection = new StringBuilder();
-            selection.append(BaseColumns._ID);
-            selection.append(" IN (");
-
-            // this tracks the order of the ids
-            long[] order = new long[cursor.getCount()];
-
-            long id = cursor.getLong(idColumn);
-            selection.append(id);
-            order[cursor.getPosition()] = id;
-
-            while (cursor.moveToNext()) {
-                selection.append(",");
-
-                id = cursor.getLong(idColumn);
-                order[cursor.getPosition()] = id;
-                selection.append(id);
-            }
-
-            selection.append(")");
-
-            // get a list of songs with the data given the selection statement
-            Cursor songCursor = SongLoader.makeSongCursor(context, selection.toString(), null);
-            if (songCursor != null) {
-                // now return the wrapped cursor to handle sorting given order
-                return new SortedLongCursor(songCursor, order, BaseColumns._ID);
-            }
-        }
-
-        return null;
     }
 }
