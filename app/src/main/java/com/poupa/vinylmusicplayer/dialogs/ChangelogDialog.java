1aee49285213a3f1d8eae4e01d149ffa71dc6ad2
Son <son.caokim@gmail.com>
2021-02-13 21:40:18 +0100

    Fix https://github.com/AdrienPoupa/VinylMusicPlayer/issues/326

--- app/src/main/java/com/poupa/vinylmusicplayer/dialogs/ChangelogDialog.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/dialogs/ChangelogDialog.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.afollestad.materialdialogs.internal.ThemeSingleton;
@@ -39,32 +41,32 @@ public class ChangelogDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final FragmentActivity activity = getActivity();
+        assert(activity != null);
+
         final View customView;
         try {
-            customView = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_web_view, null);
+            customView = LayoutInflater.from(activity).inflate(R.layout.dialog_web_view, null);
         } catch (InflateException e) {
             e.printStackTrace();
-            return new MaterialDialog.Builder(getActivity())
+            return new MaterialDialog.Builder(activity)
                     .title(android.R.string.dialog_alert_title)
                     .content("This device doesn't support web view, which is necessary to view the change log. It is missing a system component.")
                     .positiveText(android.R.string.ok)
                     .build();
         }
-        MaterialDialog dialog = new MaterialDialog.Builder(getActivity())
+        MaterialDialog dialog = new MaterialDialog.Builder(activity)
                 .title(R.string.changelog)
                 .customView(customView, false)
                 .positiveText(android.R.string.ok)
-                .showListener(dialog1 -> {
-                    if (getActivity() != null)
-                        setChangelogRead(getActivity());
-                })
+                .showListener(dialog1 -> setChangelogRead(activity))
                 .build();
 
         final WebView webView = customView.findViewById(R.id.web_view);
         try {
             // Load from vinylmusicplayer-changelog.html in the assets folder
             StringBuilder buf = new StringBuilder();
-            InputStream json = getActivity().getAssets().open("vinylmusicplayer-changelog.html");
+            InputStream json = activity.getAssets().open("vinylmusicplayer-changelog.html");
             BufferedReader in = new BufferedReader(new InputStreamReader(json, "UTF-8"));
             String str;
             while ((str = in.readLine()) != null)
@@ -72,14 +74,17 @@ public class ChangelogDialog extends DialogFragment {
             in.close();
 
             // Inject color values for WebView body background and links
-            final String backgroundColor = colorToHex(ATHUtil.resolveColor(getActivity(), R.attr.md_background_color, Color.parseColor(ThemeSingleton.get().darkTheme ? "#424242" : "#ffffff")));
+            final String backgroundColor = colorToHex(ATHUtil.resolveColor(activity, R.attr.md_background_color, Color.parseColor(ThemeSingleton.get().darkTheme ? "#424242" : "#ffffff")));
             final String contentColor = ThemeSingleton.get().darkTheme ? "#ffffff" : "#000000";
-            webView.loadData(buf.toString()
-                            .replace("{style-placeholder}",
-                                    String.format("body { background-color: %s; color: %s; }", backgroundColor, contentColor))
-                            .replace("{link-color}", colorToHex(ThemeSingleton.get().positiveColor.getDefaultColor()))
-                            .replace("{link-color-active}", colorToHex(ColorUtil.lightenColor(ThemeSingleton.get().positiveColor.getDefaultColor())))
-                    , "text/html", "UTF-8");
+            final int defaultColor = ThemeSingleton.get().positiveColor.getDefaultColor();
+            String recoloredBuf = buf.toString()
+                    .replace("{style-placeholder}",
+                            String.format("body { background-color: %s; color: %s; }", backgroundColor, contentColor))
+                    .replace("{link-color}", colorToHex(defaultColor))
+                    .replace("{link-color-active}",
+                            colorToHex(ColorUtil.lightenColor(defaultColor)));
+            String base64Buf = Base64.encodeToString(recoloredBuf.getBytes(), Base64.DEFAULT);
+            webView.loadData(base64Buf, "text/html; charset=UTF-8", "base64");
         } catch (Throwable e) {
             webView.loadData("<h1>Unable to load</h1><p>" + e.getLocalizedMessage() + "</p>", "text/html", "UTF-8");
         }
