259573ff0d0021993058e4fe786800c9ba971bf9
Son <son.caokim@gmail.com>
2021-02-25 23:31:25 +0100

    Experiment with layout hints

---
+++ app/src/main/java/com/poupa/vinylmusicplayer/auto/AutoMediaItemBuilder.java
@@ -0,0 +1,84 @@
+package com.poupa.vinylmusicplayer.auto;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaDescriptionCompat;
+
+import androidx.annotation.NonNull;
+
+import com.poupa.vinylmusicplayer.util.ImageUtil;
+
+/**
+ * @author SC (soncaokim)
+ */
+
+class AutoMediaItemBuilder {
+    final Context mContext;
+    MediaDescriptionCompat.Builder mBuilder;
+    int mFlags = 0;
+
+    AutoMediaItemBuilder(@NonNull Context context, @NonNull String path) {
+        mContext = context;
+        mBuilder = new MediaDescriptionCompat.Builder().setMediaId(path);
+    }
+
+    AutoMediaItemBuilder(@NonNull Context context, @NonNull String path, long id) {
+        this(context, AutoMediaIDHelper.createMediaID(String.valueOf(id), path));
+    }
+    @NonNull
+    AutoMediaItemBuilder title(@NonNull String title) {
+        mBuilder.setTitle(title);
+        return this;
+    }
+
+    @NonNull
+    AutoMediaItemBuilder subTitle(@NonNull String subTitle) {
+        mBuilder.setSubtitle(subTitle);
+        return this;
+    }
+
+    @NonNull
+    AutoMediaItemBuilder icon(int iconDrawableId) {
+        mBuilder.setIconBitmap(ImageUtil.createBitmap(ImageUtil.getVectorDrawable(
+                mContext.getResources(),
+                iconDrawableId,
+                mContext.getTheme()
+        )));
+        return this;
+    }
+
+    @NonNull
+    AutoMediaItemBuilder gridLayout(boolean isGrid) {
+        // Hints - see https://developer.android.com/training/cars/media#default-content-style
+        final String CONTENT_STYLE_SUPPORTED = "android.media.browse.CONTENT_STYLE_SUPPORTED";
+        final String CONTENT_STYLE_BROWSABLE_HINT = "android.media.browse.CONTENT_STYLE_BROWSABLE_HINT";
+        final String CONTENT_STYLE_PLAYABLE_HINT = "android.media.browse.CONTENT_STYLE_PLAYABLE_HINT";
+        final int CONTENT_STYLE_LIST_ITEM_HINT_VALUE = 1;
+        final int CONTENT_STYLE_GRID_ITEM_HINT_VALUE = 2;
+
+        Bundle hints = new Bundle();
+        hints.putBoolean(CONTENT_STYLE_SUPPORTED, true);
+        hints.putInt(CONTENT_STYLE_BROWSABLE_HINT, isGrid ? CONTENT_STYLE_GRID_ITEM_HINT_VALUE : CONTENT_STYLE_LIST_ITEM_HINT_VALUE);
+        hints.putInt(CONTENT_STYLE_PLAYABLE_HINT, isGrid ? CONTENT_STYLE_GRID_ITEM_HINT_VALUE : CONTENT_STYLE_LIST_ITEM_HINT_VALUE);
+
+        mBuilder.setExtras(hints);
+        return this;
+    }
+
+    @NonNull
+    AutoMediaItemBuilder flags(int value) {
+        mFlags = value;
+        return this;
+    }
+
+    @NonNull
+    MediaBrowserCompat.MediaItem build() {
+        MediaBrowserCompat.MediaItem result = new MediaBrowserCompat.MediaItem(mBuilder.build(), mFlags);
+
+        mBuilder = null;
+        mFlags = 0;
+
+        return result;
+    }
+}
--- app/src/main/java/com/poupa/vinylmusicplayer/auto/AutoMusicProvider.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/auto/AutoMusicProvider.java
@@ -3,11 +3,10 @@ package com.poupa.vinylmusicplayer.auto;
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.v4.media.MediaBrowserCompat;
-import android.support.v4.media.MediaDescriptionCompat;
-import android.text.TextUtils;
+import static android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_BROWSABLE;
+import static android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_PLAYABLE;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.poupa.vinylmusicplayer.R;
 import com.poupa.vinylmusicplayer.loader.AlbumLoader;
@@ -22,7 +21,6 @@ import com.poupa.vinylmusicplayer.model.CategoryInfo;
 import com.poupa.vinylmusicplayer.model.Playlist;
 import com.poupa.vinylmusicplayer.model.Song;
 import com.poupa.vinylmusicplayer.service.MusicService;
-import com.poupa.vinylmusicplayer.util.ImageUtil;
 import com.poupa.vinylmusicplayer.util.MusicUtil;
 import com.poupa.vinylmusicplayer.util.PreferenceUtil;
 
@@ -31,14 +29,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Beesham Sarendranauth (Beesham)
+ * @author Beesham Sarendranauth (Beesham)
+ * @author SC (soncaokim)
  */
+
 public class AutoMusicProvider {
     private final WeakReference<MusicService> mMusicService;
     private final Context mContext;
-
-    private final int BROWSABLE = MediaBrowserCompat.MediaItem.FLAG_BROWSABLE;
-    private final int PLAYABLE = MediaBrowserCompat.MediaItem.FLAG_PLAYABLE;
 
     public AutoMusicProvider(MusicService musicService) {
         mContext = musicService;
@@ -50,137 +47,138 @@ public class AutoMusicProvider {
 
         switch (mediaId) {
             case AutoMediaIDHelper.MEDIA_ID_ROOT:
-                // Follow the same library order as the full app
+                // Library sections - Follow the same order as the full app
                 ArrayList<CategoryInfo> categories = PreferenceUtil.getInstance().getLibraryCategoryInfos();
                 for (CategoryInfo categoryInfo : categories) {
                     if (categoryInfo.visible) {
                         switch (categoryInfo.category) {
                             case ALBUMS:
-                                mediaItems.add(createMediaItem(
-                                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_ALBUM,
-                                        resources.getString(R.string.albums_label),
-                                        R.drawable.ic_album_white_24dp,
-                                        BROWSABLE
-                                ));
+                                boolean albumGrid = PreferenceUtil.getInstance().getAlbumGridSize(mContext) > 1;
+                                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_ALBUM)
+                                        .title(resources.getString(R.string.albums_label))
+                                        .icon(R.drawable.ic_album_white_24dp)
+                                        .flags(FLAG_BROWSABLE)
+                                        .gridLayout(albumGrid)
+                                        .build()
+                                );
                                 break;
                             case ARTISTS:
-                                mediaItems.add(createMediaItem(
-                                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_ARTIST,
-                                        resources.getString(R.string.artists_label),
-                                        R.drawable.ic_people_white_24dp,
-                                        BROWSABLE
-                                ));
+                                boolean artistGrid = PreferenceUtil.getInstance().getArtistGridSize(mContext) > 1;
+                                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_ARTIST)
+                                        .title(resources.getString(R.string.artists_label))
+                                        .icon(R.drawable.ic_people_white_24dp)
+                                        .flags(FLAG_BROWSABLE)
+                                        .gridLayout(artistGrid)
+                                        .build()
+                                );
                                 break;
                             case PLAYLISTS:
-                                mediaItems.add(createMediaItem(
-                                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_PLAYLIST,
-                                        resources.getString(R.string.playlists_label),
-                                        R.drawable.ic_queue_music_white_24dp,
-                                        BROWSABLE
-                                ));
+                                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_PLAYLIST)
+                                        .title(resources.getString(R.string.playlists_label))
+                                        .icon(R.drawable.ic_queue_music_white_24dp)
+                                        .flags(FLAG_BROWSABLE)
+                                        .build()
+                                );
                                 break;
                             case SONGS:
-                                mediaItems.add(createMediaItem(
-                                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_SHUFFLE,
-                                        resources.getString(R.string.action_shuffle_all),
-                                        R.drawable.ic_shuffle_white_24dp,
-                                        PLAYABLE
-                                ));
+                                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_SHUFFLE)
+                                        .title(resources.getString(R.string.action_shuffle_all))
+                                        .icon(R.drawable.ic_shuffle_white_24dp)
+                                        .flags(FLAG_PLAYABLE)
+                                        .build()
+                                );
                                 break;
                         }
                     }
                 }
 
-                mediaItems.add(createMediaItem(
-                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_QUEUE,
-                        resources.getString(R.string.queue_label),
-                        R.drawable.ic_playlist_play_white_24dp,
-                        BROWSABLE
-                ));
+                // Others
+                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_QUEUE)
+                        .title(resources.getString(R.string.queue_label))
+                        .icon(R.drawable.ic_playlist_play_white_24dp)
+                        .flags(FLAG_BROWSABLE)
+                        .build()
+                );
                 break;
 
             case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_PLAYLIST:
-                // TODO Provide hints to show these as list
                 // Smart playlists
-                mediaItems.add(createMediaItem(
-                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_LAST_ADDED,
-                        resources.getString(R.string.last_added),
-                        R.drawable.ic_library_add_white_24dp,
-                        BROWSABLE
-                ));
-                mediaItems.add(createMediaItem(
-                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_HISTORY,
-                        resources.getString(R.string.history_label),
-                        R.drawable.ic_access_time_white_24dp,
-                        BROWSABLE
-                ));
-                mediaItems.add(createMediaItem(
-                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_NOT_RECENTLY_PLAYED,
-                        resources.getString(R.string.not_recently_played),
-                        R.drawable.ic_watch_later_white_24dp,
-                        BROWSABLE
-                ));
-                mediaItems.add(createMediaItem(
-                        AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_TOP_TRACKS,
-                        resources.getString(R.string.top_tracks_label),
-                        R.drawable.ic_trending_up_white_24dp,
-                        BROWSABLE
-                ));
+                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_LAST_ADDED)
+                        .title(resources.getString(R.string.last_added))
+                        .icon(R.drawable.ic_library_add_white_24dp)
+                        .flags(FLAG_BROWSABLE)
+                        .build()
+                );
+                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_HISTORY)
+                        .title(resources.getString(R.string.history_label))
+                        .icon(R.drawable.ic_access_time_white_24dp)
+                        .flags(FLAG_BROWSABLE)
+                        .build()
+                );
+                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_NOT_RECENTLY_PLAYED)
+                        .title(resources.getString(R.string.not_recently_played))
+                        .icon(R.drawable.ic_watch_later_white_24dp)
+                        .flags(FLAG_BROWSABLE)
+                        .build()
+                );
+                mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_TOP_TRACKS)
+                        .title(resources.getString(R.string.top_tracks_label))
+                        .icon(R.drawable.ic_trending_up_white_24dp)
+                        .flags(FLAG_BROWSABLE)
+                        .build()
+                );
 
                 // Static playlists
                 for (Playlist entry : PlaylistLoader.getAllPlaylists(mContext)) {
-                    mediaItems.add(createMediaItem(
-                            // Here we are appending playlist ID to the MEDIA_ID_MUSICS_BY_PLAYLIST category.
-                            // Upon browsing event, this will be handled as per the case of other playlists
-                            AutoMediaIDHelper.createMediaID(
-                                    String.valueOf(entry.id),
-                                    AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_PLAYLIST),
-                            entry.name,
-                            MusicUtil.isFavoritePlaylist(mContext, entry)
+                    // Here we are appending playlist ID to the MEDIA_ID_MUSICS_BY_PLAYLIST category.
+                    // Upon browsing event, this will be handled as per the case of other playlists
+                    mediaItems.add(new AutoMediaItemBuilder(mContext, AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_PLAYLIST, entry.id)
+                            .title(entry.name)
+                            .icon(MusicUtil.isFavoritePlaylist(mContext, entry)
                                     ? R.drawable.ic_favorite_white_24dp
-                                    : R.drawable.ic_queue_music_white_24dp,
-                            BROWSABLE
-                    ));
+                                    : R.drawable.ic_queue_music_white_24dp)
+                            .flags(FLAG_BROWSABLE)
+                            .build()
+                    );
                 }
                 break;
 
             case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_ALBUM:
-                // TODO Provide hints to show these as either list or grid, according to the preference in full app
                 for (Album entry : AlbumLoader.getAllAlbums()) {
-                    mediaItems.add(createMediaItem(
-                            AutoMediaIDHelper.createMediaID(String.valueOf(entry.getId()), mediaId),
-                            entry.getTitle(),
-                            MusicUtil.getAlbumInfoString(mContext, entry),
-                            PLAYABLE
-                    ));
+                    mediaItems.add(new AutoMediaItemBuilder(mContext, mediaId, entry.getId())
+                            .title(entry.getTitle())
+                            .subTitle(MusicUtil.getAlbumInfoString(mContext, entry))
+                            // TODO Supply the cover image
+                            .flags(FLAG_PLAYABLE)
+                            .build()
+                    );
                 }
                 break;
 
             case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_ARTIST:
-                // TODO Provide hints to show these as either list or grid, according to the preference in full app
                 for (Artist entry : ArtistLoader.getAllArtists()) {
-                    mediaItems.add(createMediaItem(
-                            AutoMediaIDHelper.createMediaID(String.valueOf(entry.getId()), mediaId),
-                            entry.getName(),
-                            MusicUtil.getArtistInfoString(mContext, entry),
-                            PLAYABLE
-                    ));
+                    mediaItems.add(new AutoMediaItemBuilder(mContext, mediaId, entry.getId())
+                            .title(entry.getName())
+                            .subTitle(MusicUtil.getArtistInfoString(mContext, entry))
+                            // TODO Supply the cover image
+                            .flags(FLAG_PLAYABLE)
+                            .build()
+                    );
                 }
                 break;
 
             case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_QUEUE:
-                // TODO Provide hints to show these as list
                 final MusicService service = mMusicService.get();
                 if (service != null) {
                     // Only show the queue starting from the currently played song
                     final List<Song> songs = truncatedList(service.getPlayingQueue(), service.getPosition());
                     for (Song s : songs) {
-                        mediaItems.add(createMediaItem(
-                                AutoMediaIDHelper.createMediaID(String.valueOf(s.id), mediaId),
-                                s.title,
-                                MusicUtil.getSongInfoString(s),
-                                PLAYABLE
-                        ));
+                        mediaItems.add(new AutoMediaItemBuilder(mContext, mediaId, s.id)
+                                .title(s.title)
+                                .subTitle(MusicUtil.getSongInfoString(s))
+                                .flags(FLAG_PLAYABLE)
+                                .build()
+                        );
                     }
                 }
                 break;
@@ -194,7 +192,6 @@ public class AutoMusicProvider {
     }
 
     private List<MediaBrowserCompat.MediaItem> getPlayListChildren(String mediaId) {
-        // TODO Provide hints to show these as list
         List<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
 
         List<Song> songs = null;
@@ -226,59 +223,16 @@ public class AutoMusicProvider {
             final List<Song> limitedSongs = truncatedList(songs, 0);
             final String path = AutoMediaIDHelper.extractCategory(mediaId);
             for (Song s : limitedSongs) {
-                mediaItems.add(createMediaItem(
-                        AutoMediaIDHelper.createMediaID(String.valueOf(s.id), path),
-                        s.title,
-                        MusicUtil.getSongInfoString(s),
-                        PLAYABLE
-                ));
+                mediaItems.add(new AutoMediaItemBuilder(mContext, path, s.id)
+                        .title(s.title)
+                        .subTitle(MusicUtil.getSongInfoString(s))
+                        .flags(FLAG_PLAYABLE)
+                        .build()
+                );
             }
         }
 
         return mediaItems;
-    }
-
-    private MediaBrowserCompat.MediaItem createMediaItem(
-            @NonNull String path,
-            @NonNull String title,
-            int iconDrawableId,
-            int flag
-    ) {
-        return createMediaItem(path, title, null, iconDrawableId, flag);
-    }
-
-    private MediaBrowserCompat.MediaItem createMediaItem(
-            @NonNull String path,
-            @NonNull String title,
-            @Nullable String subTitle,
-            int flag
-    ) {
-        return createMediaItem(path, title, subTitle, 0, flag);
-    }
-
-    private MediaBrowserCompat.MediaItem createMediaItem(
-            @NonNull String path,
-            @NonNull String title,
-            @Nullable String subTitle,
-            int iconDrawableId,
-            int flag
-    ) {
-        MediaDescriptionCompat.Builder builder = new MediaDescriptionCompat.Builder()
-                .setMediaId(path)
-                .setTitle(title);
-
-        if (!TextUtils.isEmpty(subTitle)) {
-            builder.setSubtitle(subTitle);
-        }
-
-        if (iconDrawableId != 0) {
-            builder.setIconBitmap(ImageUtil.createBitmap(ImageUtil.getVectorDrawable(
-                    mContext.getResources(),
-                    iconDrawableId,
-                    mContext.getTheme()
-            )));
-        }
-        return new MediaBrowserCompat.MediaItem(builder.build(), flag);
     }
 
     private List<Song> truncatedList(@NonNull List<Song> songs, int startPosition) {
