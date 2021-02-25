73ce97eaa5afa9957ddd310151d12f01be118bfb
Son <son.caokim@gmail.com>
2021-02-25 10:45:30 +0100

    Make the dumb playlist browseable

--- app/src/main/java/com/poupa/vinylmusicplayer/auto/AutoMusicProvider.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/auto/AutoMusicProvider.java
@@ -14,6 +14,7 @@ import com.poupa.vinylmusicplayer.loader.AlbumLoader;
 import com.poupa.vinylmusicplayer.loader.ArtistLoader;
 import com.poupa.vinylmusicplayer.loader.LastAddedLoader;
 import com.poupa.vinylmusicplayer.loader.PlaylistLoader;
+import com.poupa.vinylmusicplayer.loader.PlaylistSongLoader;
 import com.poupa.vinylmusicplayer.loader.TopAndRecentlyPlayedTracksLoader;
 import com.poupa.vinylmusicplayer.model.Album;
 import com.poupa.vinylmusicplayer.model.Artist;
@@ -22,6 +23,7 @@ import com.poupa.vinylmusicplayer.model.Playlist;
 import com.poupa.vinylmusicplayer.model.Song;
 import com.poupa.vinylmusicplayer.service.MusicService;
 import com.poupa.vinylmusicplayer.util.ImageUtil;
+import com.poupa.vinylmusicplayer.util.MusicUtil;
 import com.poupa.vinylmusicplayer.util.PreferenceUtil;
 
 import java.lang.ref.WeakReference;
@@ -33,14 +35,18 @@ import java.util.List;
  */
 public class AutoMusicProvider {
     private final WeakReference<MusicService> mMusicService;
-
     private final Context mContext;
 
-    // This is a hack - for some reason the listing of big list takes forever
-    // for Queue and Playlists, but not for Albums and Artist
-    // so instead we deliver only a portion of it to Auto
-    // TODO Drop this hack, restore the full load of different lists
-    private final int LISTING_SIZE_LIMIT = 1000;
+    private List<Song> truncatedList(@NonNull List<Song> songs, int startPosition) {
+        // As per https://developer.android.com/training/cars/media
+        // Android Auto and Android Automotive OS have strict limits on how many media items they can display in each level of the menu
+        final int LISTING_SIZE_LIMIT = 100;
+
+        final int fromPosition = Math.max(0, startPosition);
+        final int toPosition = Math.min(songs.size(), fromPosition + LISTING_SIZE_LIMIT);
+
+        return songs.subList(fromPosition, toPosition);
+    }
 
     public AutoMusicProvider(MusicService musicService) {
         mContext = musicService;
@@ -50,9 +56,10 @@ public class AutoMusicProvider {
     public List<MediaBrowserCompat.MediaItem> getChildren(String mediaId, Resources resources) {
         List<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
 
-        if (!AutoMediaIDHelper.isBrowseable(mediaId)) {
-            return mediaItems;
-        }
+//        TODO Really need this?
+//        if (!AutoMediaIDHelper.isBrowseable(mediaId)) {
+//            return mediaItems;
+//        }
 
         switch (mediaId) {
             case AutoMediaIDHelper.MEDIA_ID_ROOT:
@@ -92,8 +99,17 @@ public class AutoMusicProvider {
 
                 // Static playlists
                 for (Playlist entry : PlaylistLoader.getAllPlaylists(mContext)) {
-                    // TODO Make this browsable
-                    mediaItems.add(createPlayableMediaItem(mediaId, String.valueOf(entry.id), entry.name, null));
+                    mediaItems.add(createBrowsableMediaItem(
+                            // Here we are appending playlist ID to the MEDIA_ID_MUSICS_BY_PLAYLIST category.
+                            // Upon browsing event, this will be handled as per the case of other playlists
+                            AutoMediaIDHelper.createMediaID(
+                                    String.valueOf(entry.id),
+                                    AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_PLAYLIST),
+                            entry.name,
+                            MusicUtil.isFavoritePlaylist(mContext, entry)
+                                    ? R.drawable.ic_favorite_white_24dp
+                                    : R.drawable.ic_queue_music_white_24dp
+                    ));
                 }
                 break;
 
@@ -113,11 +129,7 @@ public class AutoMusicProvider {
                 final MusicService service = mMusicService.get();
                 if (service != null) {
                     // Only show the queue starting from the currently played song
-                    final List<Song> queue = service.getPlayingQueue();
-                    final int fromPosition = Math.max(0, service.getPosition());
-                    final int toPosition = Math.min(queue.size() - 1, fromPosition + LISTING_SIZE_LIMIT);
-                    final List<Song> songs = queue.subList(fromPosition, toPosition);
-
+                    final List<Song> songs = truncatedList(service.getPlayingQueue(), service.getPosition());
                     for (Song s : songs) {
                         final String artists = MultiValuesTagUtil.infoString(s.artistNames);
                         mediaItems.add(createPlayableMediaItem(mediaId, String.valueOf(s.id), s.title, artists));
@@ -125,33 +137,49 @@ public class AutoMusicProvider {
                 }
                 break;
 
-            default:
-                List<Song> songs = null;
-                switch (mediaId) {
-                    case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_LAST_ADDED:
-                        songs = LastAddedLoader.getLastAddedSongs();
-                        break;
-                    case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_HISTORY:
-                        songs = TopAndRecentlyPlayedTracksLoader.getRecentlyPlayedTracks(mContext);
-                        break;
-                    case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_NOT_RECENTLY_PLAYED:
-                        songs = TopAndRecentlyPlayedTracksLoader.getNotRecentlyPlayedTracks(mContext);
-                        break;
-                    case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_TOP_TRACKS:
-                        songs = TopAndRecentlyPlayedTracksLoader.getTopTracks(mContext);
-                        break;
-                }
-                if (songs != null) {
-                    final int fromPosition = 0;
-                    final int toPosition = Math.min(songs.size() - 1, fromPosition + LISTING_SIZE_LIMIT);
-                    final List<Song> limitedSongs = songs.subList(fromPosition, toPosition);
+            default: // We get to the case of (smart/dumb) playlists here
+                mediaItems.addAll(getPlayListChildren(mediaId));
+                break;
+        }
 
-                    for (Song s : limitedSongs) {
-                        final String artists = MultiValuesTagUtil.infoString(s.artistNames);
-                        mediaItems.add(createPlayableMediaItem(mediaId, String.valueOf(s.id), s.title, artists));
+        return mediaItems;
+    }
+
+    private List<MediaBrowserCompat.MediaItem> getPlayListChildren(String mediaId) {
+        List<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
+
+        List<Song> songs = null;
+        switch (mediaId) {
+            case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_LAST_ADDED:
+                songs = LastAddedLoader.getLastAddedSongs();
+                break;
+            case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_HISTORY:
+                songs = TopAndRecentlyPlayedTracksLoader.getRecentlyPlayedTracks(mContext);
+                break;
+            case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_NOT_RECENTLY_PLAYED:
+                songs = TopAndRecentlyPlayedTracksLoader.getNotRecentlyPlayedTracks(mContext);
+                break;
+            case AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_TOP_TRACKS:
+                songs = TopAndRecentlyPlayedTracksLoader.getTopTracks(mContext);
+                break;
+            default:
+                final String mediaCategory = AutoMediaIDHelper.extractCategory(mediaId);
+                if (mediaCategory.equals(AutoMediaIDHelper.MEDIA_ID_MUSICS_BY_PLAYLIST)) {
+                    try {
+                        long playListId = Long.parseLong(AutoMediaIDHelper.extractMusicID(mediaId));
+                        songs = PlaylistSongLoader.getPlaylistSongList(mContext, playListId);
                     }
+                    catch (NumberFormatException ignored) {}
                 }
                 break;
+        }
+        if (songs != null) {
+            final List<Song> limitedSongs = truncatedList(songs, 0);
+            final String mediaCategory = AutoMediaIDHelper.extractCategory(mediaId);
+            for (Song s : limitedSongs) {
+                final String artists = MultiValuesTagUtil.infoString(s.artistNames);
+                mediaItems.add(createPlayableMediaItem(mediaCategory, String.valueOf(s.id), s.title, artists));
+            }
         }
 
         return mediaItems;
