ac60deb17d65cc76b04d8a8486af485a2852b9b1
Ansh <ansh.sancheti@gmail.com>
2021-04-04 21:12:07 -0700

    Issue #84: Adding ability to fast forward and rewind by holding next and previous song buttons

---
+++ app/src/main/java/com/poupa/vinylmusicplayer/helper/PrevNextButtonOnTouchListener.java
@@ -0,0 +1,63 @@
+package com.poupa.vinylmusicplayer.helper;
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.os.Handler;
+import android.view.View.OnClickListener;
+
+public class PrevNextButtonOnTouchListener implements View.OnTouchListener {
+    private Handler handler = new Handler();
+
+    private int initialInterval;
+    private final int normalInterval;
+    private final View.OnGenericMotionListener genericMotionListener;
+    private View touchedView;
+    private boolean wasHeld;
+
+    private Runnable handlerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if(touchedView.isEnabled()) {
+                wasHeld = true;
+                handler.postDelayed(this, normalInterval);
+                genericMotionListener.onGenericMotion(touchedView, MotionEvent.obtain(0,0,MotionEvent.ACTION_DOWN,0,0,0));
+            } else {
+                // if the view was disabled by the clickListener, remove the callback
+                handler.removeCallbacks(handlerRunnable);
+                touchedView.setPressed(false);
+                touchedView = null;
+                wasHeld = false;
+            }
+        }
+    };
+
+    public PrevNextButtonOnTouchListener(int initialInterval, int normalInterval,
+                                       View.OnGenericMotionListener genericMotionListener) {
+        this.initialInterval = initialInterval;
+        this.normalInterval = normalInterval;
+        this.genericMotionListener = genericMotionListener;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch(motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                handler.removeCallbacks(handlerRunnable);
+                handler.postDelayed(handlerRunnable, initialInterval);
+                touchedView = view;
+                touchedView.setPressed(true);
+                return true;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                if(!wasHeld) {
+                    genericMotionListener.onGenericMotion(touchedView, MotionEvent.obtain(0,0,MotionEvent.ACTION_CANCEL,0,0,0));
+                }
+                handler.removeCallbacks(handlerRunnable);
+                touchedView.setPressed(false);
+                touchedView = null;
+                wasHeld = false;
+                return true;
+        }
+        return false;
+    }
+}
