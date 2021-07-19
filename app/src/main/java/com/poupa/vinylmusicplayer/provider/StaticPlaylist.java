7ff27c9f6441c716e9c9a02b231a65e5b2b1f4fb
Son <son.caokim@gmail.com>
2021-07-19 21:29:34 +0200

    Fix class cast error

--- app/src/main/java/com/poupa/vinylmusicplayer/provider/StaticPlaylist.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/provider/StaticPlaylist.java
@@ -84,4 +84,19 @@ public class StaticPlaylist extends PreferencesBackedSongList {
         // TODO Dont use hashcode as a substitude for id
         return new Playlist(name.hashCode(), name);
     }
+
+    @Override
+    @NonNull
+    public ArrayList<Song> asSongs() {
+        // TODO Review this suboptimal implementation
+        // Repackage the song lists as a list of PlaylistSong
+        final ArrayList<Song> songs = super.asSongs();
+        final long playlistId = asPlaylist().id;
+
+        ArrayList<Song> result = new ArrayList<>();
+        for (Song song : songs) {
+            result.add(new PlaylistSong(song, playlistId, result.size()));
+        }
+        return result;
+    }
 }
