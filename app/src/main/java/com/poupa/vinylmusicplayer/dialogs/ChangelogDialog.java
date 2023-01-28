eb7632485fdd20edf8c7a887adf45639eef2707d
Son <son.caokim@gmail.com>
2023-01-28 22:36:20 +0100

    Have a single source of change log (instead of .md vs .html), and present to the user the .md version.

--- app/src/main/java/com/poupa/vinylmusicplayer/dialogs/ChangelogDialog.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/dialogs/ChangelogDialog.java
@@ -5,28 +5,22 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Base64;
-import android.view.InflateException;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.webkit.WebView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
 
 import com.afollestad.materialdialogs.MaterialDialog;
-import com.afollestad.materialdialogs.internal.ThemeSingleton;
-import com.kabouzeid.appthemehelper.util.ATHUtil;
-import com.kabouzeid.appthemehelper.util.ColorUtil;
 import com.poupa.vinylmusicplayer.R;
 import com.poupa.vinylmusicplayer.util.PreferenceUtil;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Aidan Follestad (afollestad)
@@ -44,17 +38,7 @@ public class ChangelogDialog extends DialogFragment {
         final FragmentActivity activity = getActivity();
         assert(activity != null);
 
-        final View customView;
-        try {
-            customView = LayoutInflater.from(activity).inflate(R.layout.dialog_web_view, null);
-        } catch (InflateException e) {
-            e.printStackTrace();
-            return new MaterialDialog.Builder(activity)
-                    .title(android.R.string.dialog_alert_title)
-                    .content("This device doesn't support web view, which is necessary to view the change log. It is missing a system component.")
-                    .positiveText(android.R.string.ok)
-                    .build();
-        }
+        final View customView = getLayoutInflater().inflate(R.layout.dialog_changelog_view, null);
         MaterialDialog dialog = new MaterialDialog.Builder(activity)
                 .title(R.string.changelog)
                 .customView(customView, false)
@@ -62,31 +46,22 @@ public class ChangelogDialog extends DialogFragment {
                 .showListener(dialog1 -> setChangelogRead(activity))
                 .build();
 
-        final WebView webView = customView.findViewById(R.id.web_view);
+        final TextView textView = customView.findViewById(R.id.text_view);
         try {
-            // Load from vinylmusicplayer-changelog.html in the assets folder
+            // Load from CHANGELOG.md in the assets folder
             StringBuilder buf = new StringBuilder();
-            InputStream json = activity.getAssets().open("vinylmusicplayer-changelog.html");
-            BufferedReader in = new BufferedReader(new InputStreamReader(json, "UTF-8"));
+            InputStream json = activity.getAssets().open("CHANGELOG.md");
+            BufferedReader in = new BufferedReader(new InputStreamReader(json, StandardCharsets.UTF_8));
             String str;
-            while ((str = in.readLine()) != null)
+            while ((str = in.readLine()) != null) {
                 buf.append(str);
+                buf.append('\n');
+            }
             in.close();
 
-            // Inject color values for WebView body background and links
-            final String backgroundColor = colorToHex(ATHUtil.resolveColor(activity, R.attr.md_background_color, Color.parseColor(ThemeSingleton.get().darkTheme ? "#424242" : "#ffffff")));
-            final String contentColor = ThemeSingleton.get().darkTheme ? "#ffffff" : "#000000";
-            final int defaultColor = ThemeSingleton.get().positiveColor.getDefaultColor();
-            String recoloredBuf = buf.toString()
-                    .replace("{style-placeholder}",
-                            String.format("body { background-color: %s; color: %s; }", backgroundColor, contentColor))
-                    .replace("{link-color}", colorToHex(defaultColor))
-                    .replace("{link-color-active}",
-                            colorToHex(ColorUtil.lightenColor(defaultColor)));
-            String base64Buf = Base64.encodeToString(recoloredBuf.getBytes(), Base64.DEFAULT);
-            webView.loadData(base64Buf, "text/html; charset=UTF-8", "base64");
+            textView.setText(buf.toString());
         } catch (Throwable e) {
-            webView.loadData("<h1>Unable to load</h1><p>" + e.getLocalizedMessage() + "</p>", "text/html", "UTF-8");
+            textView.setText("Unable to load change log\n" + e.getLocalizedMessage());
         }
         return dialog;
     }
