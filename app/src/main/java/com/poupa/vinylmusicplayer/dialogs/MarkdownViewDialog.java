5fb4c38a839d2e0776a14ac38748d49597747e47
Son <son.caokim@gmail.com>
2023-02-22 13:48:36 +0100

    Refactoring of MarkdownViewDialog, allowing caller to freely call setMarkdownContent* before or after the dialog is shown

--- app/src/main/java/com/poupa/vinylmusicplayer/dialogs/MarkdownViewDialog.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/dialogs/MarkdownViewDialog.java
@@ -1,18 +1,14 @@
 package com.poupa.vinylmusicplayer.dialogs;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.Dialog;
+import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentActivity;
+import androidx.annotation.UiThread;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.poupa.vinylmusicplayer.R;
@@ -26,63 +22,48 @@ import java.nio.charset.StandardCharsets;
 import io.noties.markwon.AbstractMarkwonPlugin;
 import io.noties.markwon.Markwon;
 import io.noties.markwon.core.MarkwonTheme;
+import io.noties.markwon.html.HtmlPlugin;
 import io.noties.markwon.image.glide.GlideImagesPlugin;
 
 /**
  * @author SC (soncaokim)
  */
-public class MarkdownViewDialog extends DialogFragment {
-    @NonNull
-    private final String assetName;
+public class MarkdownViewDialog extends MaterialDialog {
+    private String markdownContent;
 
-    public MarkdownViewDialog(@NonNull final String assetName) {
-        super();
-        this.assetName = assetName;
+    protected MarkdownViewDialog(Builder builder) {
+        super(builder);
     }
 
-    @SuppressLint("InflateParams")
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final FragmentActivity activity = requireActivity();
+    public MarkdownViewDialog setMarkdownContent(@NonNull final Context context, @NonNull final String content) {
+        markdownContent = content;
 
-        final View customView = getLayoutInflater().inflate(R.layout.dialog_markdown_view, null);
-        MaterialDialog dialog = new MaterialDialog.Builder(activity)
-                .customView(customView, false)
-                .positiveText(android.R.string.ok)
-                .build();
+        if (isShowing()) {
+            final Markwon markwon = Markwon.builder(context)
+                    .usePlugin(GlideImagesPlugin.create(context)) // image loader
+                    .usePlugin(HtmlPlugin.create()) // basic Html tags
+                    .usePlugin(new GithubLinkify())
+                    .usePlugin(new CustomStyles(context))
+                    .build();
+            final TextView markdownView = getCustomView().findViewById(R.id.markdown_view);
+            markwon.setMarkdown(markdownView, markdownContent);
+        } else {
+            setOnShowListener(dialog -> ((MarkdownViewDialog)dialog).setMarkdownContent(context, markdownContent));
+        }
 
-        final TypedValue typedColor = new TypedValue();
-        getActivity().getTheme().resolveAttribute(R.attr.dividerColor, typedColor, true);
+        return this;
+    }
 
-        final Markwon markwon = Markwon.builder(activity)
-                .usePlugin(GlideImagesPlugin.create(activity)) // image loader
-                .usePlugin(new GithubLinkify())
-                .usePlugin(new AbstractMarkwonPlugin() {
-                    @Override
-                    public void configureTheme(@NonNull MarkwonTheme.Builder builder) {
-                        builder
-                                .headingBreakColor(Color.parseColor("#00ffffff"))
-                                .thematicBreakColor(typedColor.data)
-                                .thematicBreakHeight(2)
-                                .bulletWidth(12)
-                                .headingTextSizeMultipliers(
-                                        new float[] { 2.F, 1.5F, 1F, .83F, .67F, .55F });
-                    }
-                })
-                .build();
-        final TextView markdownView = customView.findViewById(R.id.markdown_view);
-        StringBuilder buf = loadFileFromAssets(activity, assetName);
-        markwon.setMarkdown(markdownView, buf.toString());
-
-        return dialog;
+    public MarkdownViewDialog setMarkdownContentFromAsset(@NonNull final Context context, @NonNull final String assetName) {
+        final String content = loadFileFromAssets(context, assetName);
+        return setMarkdownContent(context, content);
     }
 
     @NonNull
-    private StringBuilder loadFileFromAssets(@NonNull Activity activity, @NonNull final String name) {
+    private String loadFileFromAssets(@NonNull final Context context, @NonNull final String name) {
         StringBuilder buf = new StringBuilder();
         try {
-            InputStream json = activity.getAssets().open(name);
+            InputStream json = context.getAssets().open(name);
             BufferedReader in = new BufferedReader(new InputStreamReader(json, StandardCharsets.UTF_8));
             String str;
             while ((str = in.readLine()) != null) {
@@ -96,7 +77,23 @@ public class MarkdownViewDialog extends DialogFragment {
                 .append("\n\n")
                 .append(ioe.getLocalizedMessage());
         }
-        return buf;
+        return buf.toString();
+    }
+
+    public static class Builder extends MaterialDialog.Builder {
+        public Builder(@NonNull Context context) {
+            super(context);
+        }
+
+        @Override
+        @UiThread
+        public MarkdownViewDialog build() {
+            final View customView = LayoutInflater.from(context).inflate(R.layout.dialog_markdown_view, null);
+            customView(customView, false);
+            positiveText(android.R.string.ok);
+
+            return new MarkdownViewDialog(this);
+        }
     }
 
     // Github link shortener plugin
@@ -111,6 +108,27 @@ public class MarkdownViewDialog extends DialogFragment {
                     .replaceAll(
                             "(https://github.com/VinylMusicPlayer/VinylMusicPlayer/pull/)([0-9]+)",
                             "[PR #$2]($1$2)");
+        }
+    }
+
+    private static class CustomStyles extends AbstractMarkwonPlugin {
+        private @NonNull final Context context;
+        public CustomStyles(@NonNull final Context context) {
+            super();
+            this.context = context;
+        }
+
+        @Override
+        public void configureTheme(@NonNull MarkwonTheme.Builder builder) {
+            final TypedValue typedColor = new TypedValue();
+            context.getTheme().resolveAttribute(R.attr.dividerColor, typedColor, true);
+
+            builder.headingBreakColor(Color.parseColor("#00ffffff"))
+                    .thematicBreakColor(typedColor.data)
+                    .thematicBreakHeight(2)
+                    .bulletWidth(12)
+                    .headingTextSizeMultipliers(
+                            new float[] { 2.F, 1.5F, 1F, .83F, .67F, .55F });
         }
     }
 }
