261630251ebe149e3bb0825aacc8d78558c32757
Son <son.caokim@gmail.com>
2023-06-17 12:42:35 +0200

    Rollback unused code

--- app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/AbsMusicServiceFragment.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/ui/fragments/AbsMusicServiceFragment.java
@@ -1,71 +1,21 @@
 package com.poupa.vinylmusicplayer.ui.fragments;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.poupa.vinylmusicplayer.interfaces.MusicServiceEventListener;
 import com.poupa.vinylmusicplayer.ui.activities.base.AbsMusicServiceActivity;
-import com.poupa.vinylmusicplayer.util.SAFUtil;
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
 public class AbsMusicServiceFragment extends Fragment implements MusicServiceEventListener {
     private AbsMusicServiceActivity activity;
-
-    ActivityResultLauncher<Uri> SAFTreePicker;
-    public ActivityResultLauncher<Intent> LollipopSAFGuide;
-    public ActivityResultLauncher<String> KitkatSAFFilePicker;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // TODO Isolate in a named method
-        SAFTreePicker = registerForActivityResult(
-                new ActivityResultContracts.OpenDocumentTree() {
-                    @NonNull
-                    @Override
-                    public Intent createIntent(@NonNull Context context, Uri input) {
-                        return super.createIntent(context, input)
-                                .putExtra("android.content.extra.SHOW_ADVANCED", true);
-                    }
-                },
-                resultUri -> SAFUtil.saveTreeUri(this.requireContext(), resultUri)
-        );
-
-        LollipopSAFGuide = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == Activity.RESULT_OK) {
-                        SAFTreePicker.launch(null);
-                    }
-                });
-
-        KitkatSAFFilePicker = registerForActivityResult(
-                new ActivityResultContracts.CreateDocument("audio/*") { // or 'audio/mpegurl
-                    @NonNull
-                    @Override
-                    public Intent createIntent(@NonNull Context context, @NonNull String input) {
-                        return super.createIntent(context, input)
-                                .addCategory(Intent.CATEGORY_OPENABLE)
-                                .putExtra("android.content.extra.SHOW_ADVANCED", true);
-                    }
-                },
-                resultUri -> {
-                    // TODO Nothing?
-                });
-    }
 
     @Override
     public void onAttach(@NonNull Context context) {
