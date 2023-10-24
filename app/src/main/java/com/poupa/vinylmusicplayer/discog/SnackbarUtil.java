032c48512a3221fc02451c828986652f85c1e0f4
Son <son.caokim@gmail.com>
2023-10-24 17:07:18 +0200

    API compat fixes

--- app/src/main/java/com/poupa/vinylmusicplayer/discog/SnackbarUtil.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/discog/SnackbarUtil.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.poupa.vinylmusicplayer.App;
