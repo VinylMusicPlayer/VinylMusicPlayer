2d414484d5f666a8926099266c251a51398f378c
Andreas Lechner <lechner396@gmail.com>
2024-02-04 20:47:09 +0100

    Delete unnecessary code.

--- app/src/main/java/com/poupa/vinylmusicplayer/preferences/ExportSettingsPreferenceDialog.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/preferences/ExportSettingsPreferenceDialog.java
@@ -4,58 +4,24 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.media.audiofx.AudioEffect;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.preference.PreferenceManager;
-
-import com.google.gson.Gson;
-import com.poupa.vinylmusicplayer.App;
 import com.poupa.vinylmusicplayer.R;
-import com.poupa.vinylmusicplayer.provider.BlacklistStore;
-import com.poupa.vinylmusicplayer.util.OopsHandler;
-import com.poupa.vinylmusicplayer.util.SafeToast;
-import com.poupa.vinylmusicplayer.util.PreferenceUtil;
 import com.poupa.vinylmusicplayer.util.SharedPreferencesExporter;
-import com.poupa.vinylmusicplayer.util.StringUtil;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Path;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Map;
-
-import static android.app.Activity.RESULT_OK;
-import static android.content.Context.GRAMMATICAL_INFLECTION_SERVICE;
-import static com.poupa.vinylmusicplayer.util.SharedPreferencesExporter.WRITE_REQUEST_CODE;
 
 /**
  * @author Andreas Lechner ()
@@ -69,9 +35,6 @@ public class ExportSettingsPreferenceDialog extends DialogFragment {
 
     @NonNull private final String preferenceKey;
     private Context context;
-    //private final int WRITE_REQUEST_CODE = 1;
-
-
 
     public ExportSettingsPreferenceDialog(@NonNull String preference) {
         preferenceKey = preference;
@@ -83,9 +46,6 @@ public class ExportSettingsPreferenceDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-        Gson gson = new Gson(); // com.google.gson.Gson
-        //String jsonFromMap = gson.toJson(allPreferences);
         this.context = this.getContext();
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context);
         final Activity activity = requireActivity();
@@ -100,23 +60,14 @@ public class ExportSettingsPreferenceDialog extends DialogFragment {
         editText.setText(filename, TextView.BufferType.EDITABLE);
         alert.setPositiveButton(android.R.string.ok, (dialog, id) -> {
             // User taps OK button.
-            //createFile(filename);
-            //this.context.startActivity(new Intent().setClass(this.context, SharedPreferencesExporter.class).putExtra("filename", editText.getText()));
             SharedPreferencesExporter.start(this.context, editText.getText().toString());
-            //buttonCreateFile(activity, filename);//, jsonFromMap);
-            //preferences;
-            //SharedPreferencesExporter spe = new SharedPreferencesExporter();//App.getStaticContext());
-            //spe.export();
-
         });
         alert.setNegativeButton(android.R.string.cancel, (dialog, id) -> {
             // User cancels the dialog.
         });
 
         return alert.create();
-
     }
-
 
     private static String getCurrentFormattedDateTime() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault());
@@ -134,88 +85,4 @@ public class ExportSettingsPreferenceDialog extends DialogFragment {
         createDocument.addCategory(Intent.CATEGORY_OPENABLE);
         //Log.i("LogFileinfo: ", createDocument.)
     }
-
-    private Intent createFileIntent(String filename) {
-        //Intent intent = null;
-        //if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);//, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
-        //intent = new Intent(Intent.ACTION_);//, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
-        //}
-
-        //Context context = getContext();
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TITLE, filename);
-
-        //this.startActivity(intent);
-
-        return intent;
-    }
-
-    private String exportSharedPreferences() {
-        StringBuilder stringBuilder = new StringBuilder();
-        Map<String, ?> prefsMap = sharedPreferences.getAll();
-        for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
-            stringBuilder.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-        }
-
-        return stringBuilder.toString();
-    }
-
-    public void buttonCreateFile(Activity activity, String filename) { //, String content) {
-
-        //intent.setAction(Intent.AC);
-        //intent.putExtra(Intent.EXTRA_TEXT, content);
-        //intent.setType("*/*");
-        //String content = this.exportSharedPreferences();
-        //Intent intent = createFileIntent(filename);
-        //createFileIntent(filename);
-        //activity.startActivityForResult(intent, WRITE_REQUEST_CODE);
-        //this.registerForActivityResult(intent);
-        /*ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.CreateDocument(),
-                uri -> {
-                    try {
-                        ParcelFileDescriptor pfd = activity.getContentResolver().openFileDescriptor(uri, "w"); //intent.getData(), "w");
-                        FileOutputStream fileOutputStream = new FileOutputStream(pfd.getFileDescriptor());
-                        fileOutputStream.write(content.getBytes());
-                        fileOutputStream.close();
-                        pfd.close();
-                    } catch (IOException e) {
-                        Log.e(ExportSettingsPreferenceDialog.class.getName(), e.getMessage());
-                    }
-                });*/
-
-
-
-        //createDocumentLauncher.launch(filename);
-
-        Log.i(ExportSettingsPreferenceDialog.class.getName(), "------createDocumentLauncher Ende");
-
-
-        //Log.i(ExportSettingsPreferenceDialog.class.getName(), content);
-
-
-        //Log.i(ExportSettingsPreferenceDialog.class.getName(), intent.getData().toString());
-    }
-
-    private void doActivityThings(Activity activity, Intent intent, String content) {
-
-    }
-
-
-
-
-    public void buttonOpenFile(View view){
-        Intent intent = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            intent = new Intent(Intent.ACTION_VIEW); //, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
-        }
-        //intent.setType("application/pdf");
-        intent.setType("*/*");
-        this.startActivity(intent);
-    }
-
-
-
-
 }
--- app/src/main/java/com/poupa/vinylmusicplayer/util/SharedPreferencesExporter.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/util/SharedPreferencesExporter.java
@@ -1,38 +1,19 @@
 package com.poupa.vinylmusicplayer.util;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.ParcelFileDescriptor;
-import android.util.Log;
-
-import com.poupa.vinylmusicplayer.App;
-import com.poupa.vinylmusicplayer.ui.activities.SettingsActivity;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.Map;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.documentfile.provider.DocumentFile;
 import androidx.preference.PreferenceManager;
-
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class SharedPreferencesExporter extends AppCompatActivity {
 
@@ -40,37 +21,9 @@ public class SharedPreferencesExporter extends AppCompatActivity {
     public static final int WRITE_REQUEST_CODE = 1;
 
     private Context context;
-    private String content;
     private ActivityResultLauncher<String> exportFilePicker;
-
-    public SharedPreferencesExporter() { //(Context context) {//, Activity activity) {
-        //this.context = this.getApplicationContext();
-        //this.content = content;
-    }
-
-    private void setContent(String content) {
-        this.content = content;
-    }
-
-    private String getContent() {
-        return this.content;
-    }
-
     private SharedPreferences sharedPreferences;
 
-    /*
-    private final ActivityResultLauncher<String> createDocumentLauncher =
-            registerForActivityResult(new ActivityResultContracts.CreateDocument("text/plain"),
-                    result -> {
-                        if (result != null) {
-                            try {
-                                exportSharedPreferences(result);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-     */
     public static void start(Context context, String filename) {
         Intent intent = new Intent(context, SharedPreferencesExporter.class);
         intent.putExtra("filename", filename);
@@ -88,62 +41,13 @@ public class SharedPreferencesExporter extends AppCompatActivity {
             if(result != null) {
                 writeToExportFile(result);
             }
-            //this.context.startActivity(new Intent().setClass(this.context, SettingsActivity.class).addFlags(FLAG_ACTIVITY_NEW_TASK)) ;
+            // Finishes the last activity to return to the settings activity.
             this.finish();
         });
         exportFilePicker.launch(b.getString("filename"));
-        //exportSharedPreferencesWithPermission();
     }
 
-    private static String getCurrentFormattedDateTime() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault());
-        return simpleDateFormat.format(new Date(System.currentTimeMillis()));
-    }
-
-    private static String getExportSettingsFilename() {
-        return "vinylmusicplayer-settings_"+ getCurrentFormattedDateTime();
-    }
-
-    private void exportSharedPreferencesWithPermission() {
-        //createDocumentLauncher.launch(this.getExportSettingsFilename());
-    }
-
-    private void exportSharedPreferences(Uri uri) throws IOException {
-        OutputStream outputStream = getContentResolver().openOutputStream(uri);
-        if (outputStream != null) {
-            StringBuilder stringBuilder = new StringBuilder();
-            Map<String, ?> prefsMap = sharedPreferences.getAll();
-            for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
-                stringBuilder.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-            }
-
-            outputStream.write(stringBuilder.toString().getBytes());
-            outputStream.close();
-        }
-    }
-
-    public void export() { //Context context) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context);
-        String sharedPrefsString = sharedPreferences.getAll().toString();
-        this.setContent(sharedPrefsString);
-
-        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TITLE, "shared_prefs.txt");
-        intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
-
-        this.context.startActivity(intent);
-        this.startActivityForResult(intent, 1);
-
-        //Uri uri = intent.getData();
-        //Log.i(SharedPreferencesExporter.class.getName(), uri.toString());
-        //Log.i(SharedPreferencesExporter.class.getName(), "Bis hier her");
-
-    }
-
-    private void writeToExportFile(Uri location)
-    {
+    private void writeToExportFile(Uri location) {
         StringBuilder stringBuilder = new StringBuilder();
         Map<String, ?> prefsMap = sharedPreferences.getAll();
         for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
@@ -151,47 +55,20 @@ public class SharedPreferencesExporter extends AppCompatActivity {
         }
 
         // Write all lines in the export file
-        try
-        {
+        try {
             // Try to open the file
-            ParcelFileDescriptor file = this.context.getContentResolver().openFileDescriptor(location, "w") ;
-            if(file == null) return;
+            ParcelFileDescriptor file = this.context.getContentResolver().openFileDescriptor(location, "w");
+            if (file == null) return;
 
             // Write all lines in the file
-            FileWriter writer = new FileWriter(file.getFileDescriptor()) ;
+            FileWriter writer = new FileWriter(file.getFileDescriptor());
             writer.write(stringBuilder.toString());
-            writer.close() ;
-            file.close() ;
-        }
-        catch(IOException exception)
-        {
+            writer.close();
+            file.close();
+        } catch (IOException exception) {
             // An error happened while writing the line
             OopsHandler.collectStackTrace(exception);
         }
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == WRITE_REQUEST_CODE) {
-            try {
-                FileOutputStream fileOutupStream = (FileOutputStream) getContentResolver().openOutputStream(data.getData());
-                try {
-                    fileOutupStream.write(this.getContent().getBytes());
-                    fileOutupStream.flush();
-                    fileOutupStream.close();
-                    //Toast.makeText(this, "saved " + fileName, Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    //Toast.makeText(this, "something went wrong" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
-                }
-            } catch (FileNotFoundException e) {
-
-            }
-
-        }
-    }
-
-
 }
 
