9a546943eab043fff482b5295160eb09dc908f8b
Son <son.caokim@gmail.com>
2024-04-06 13:38:50 +0200

    Fallback to cover image scanned and stored in MediaStore

--- app/src/main/java/com/poupa/vinylmusicplayer/glide/audiocover/AbsCoverFetcher.java
+++ app/src/main/java/com/poupa/vinylmusicplayer/glide/audiocover/AbsCoverFetcher.java
@@ -1,10 +1,31 @@
 package com.poupa.vinylmusicplayer.glide.audiocover;
 
+import android.content.ContentUris;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.util.Size;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.data.DataFetcher;
+import com.poupa.vinylmusicplayer.App;
+import com.poupa.vinylmusicplayer.model.Song;
+import com.poupa.vinylmusicplayer.util.AutoCloseAudioFile;
+import com.poupa.vinylmusicplayer.util.OopsHandler;
+import com.poupa.vinylmusicplayer.util.SAFUtil;
+import com.poupa.vinylmusicplayer.util.Util;
 
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.tag.images.Artwork;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,7 +37,7 @@ import java.io.InputStream;
  * @author SC (soncaokim)
  */
 public abstract class AbsCoverFetcher implements DataFetcher<InputStream> {
-    private FileInputStream stream;
+    private InputStream stream;
 
     @NonNull
     @Override
@@ -30,22 +51,79 @@ public abstract class AbsCoverFetcher implements DataFetcher<InputStream> {
         return DataSource.LOCAL;
     }
 
-    private static final String[] FALLBACKS =
-            {"cover.jpg", "album.jpg", "folder.jpg", "cover.png", "album.png", "folder.png"};
-
-    protected InputStream fallback(String path) throws FileNotFoundException {
-        return fallback(new File(path));
+    @Nullable
+    InputStream loadCoverFromAudioTags(@NonNull final Song song) {
+        try (final AutoCloseAudioFile audio = SAFUtil.loadReadOnlyAudioFile(App.getStaticContext(), song)) {
+            if (audio == null) {return null;}
+            return loadCoverFromAudioTags(audio.get());
+        } catch (final Exception e) {
+            OopsHandler.collectStackTrace(e);
+        }
+        return null;
     }
 
-    protected InputStream fallback(File file) throws FileNotFoundException {
-        // Look for album art in external files
-        File parent = file.getParentFile();
-        for (String fallback : FALLBACKS) {
-            File cover = new File(parent, fallback);
-            if (cover.exists()) {
-                return stream = new FileInputStream(cover);
+    @Nullable
+    InputStream loadCoverFromAudioTags(@NonNull final File file) {
+        try {
+            final AudioFile audio = SAFUtil.loadAudioFile(file);
+            if (audio == null) {return null;}
+            return loadCoverFromAudioTags(audio);
+        } catch (final Exception e) {
+            OopsHandler.collectStackTrace(e);
+        }
+        return null;
+    }
+
+    @Nullable
+    private InputStream loadCoverFromAudioTags(@NonNull final AudioFile audio) {
+        final Artwork art = audio.getTag().getFirstArtwork();
+        if (art != null) {
+            final byte[] imageData = art.getBinaryData();
+            return stream = new ByteArrayInputStream(imageData);
+        }
+        return null;
+    }
+
+    @Nullable
+    InputStream loadCoverFromMediaStore(@NonNull final Song song) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            final Uri uri = ContentUris.withAppendedId(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, song.albumId);
+            try {
+                final Context context = App.getStaticContext();
+                final Point screenSize = Util.getScreenSize(context);
+                final int coverSize = Math.min(screenSize.x, screenSize.y);
+                final Bitmap cover = context.getContentResolver().loadThumbnail(uri, new Size(coverSize, coverSize), null);
+
+                // Hideous way to transfer the Bitmap data to an InputStream
+                ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+                cover.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+                return stream = new ByteArrayInputStream(ostream.toByteArray());
+            } catch (final FileNotFoundException ignored) {
+                // No album
+            } catch (final IOException error) {
+                OopsHandler.collectStackTrace(error);
             }
         }
+        return null;
+    }
+
+    private static final String[] FOLDER_IMAGE_FALLBACKS = {
+            "cover.jpg", "album.jpg", "folder.jpg",
+            "cover.png", "album.png", "folder.png"
+    };
+
+    @Nullable
+    InputStream loadCoverFromFolderImage(@NonNull final File file) {
+        // Look for album art in external files
+        try {
+            final File parent = file.getParentFile();
+            for (final String fallback : FOLDER_IMAGE_FALLBACKS) {
+                final File cover = new File(parent, fallback);
+                if (cover.exists()) {
+                    return stream = new FileInputStream(cover);
+                }
+            }
+        } catch (final FileNotFoundException ignored) {}
         return null;
     }
 
@@ -55,7 +133,7 @@ public abstract class AbsCoverFetcher implements DataFetcher<InputStream> {
         if (stream != null) {
             try {
                 stream.close();
-            } catch (IOException ignore) {
+            } catch (final IOException ignore) {
                 // can't do much about it
             }
         }
